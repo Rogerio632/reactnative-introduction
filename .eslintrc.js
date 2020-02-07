1eedc87805437fdbd7998f79091e08e88bd9da05
Rogério Filho <rogeriofilho632@gmail.com>
2020-02-07 10:48:59 -0300

    feat: configure default way to code

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  /**eslint vai entender as últimas versões do JS
+   * com essa integração com o babel
+   */
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx']
+      }
+    ],
+    'import/prefer-default-export': 'off'
+  },
+};
